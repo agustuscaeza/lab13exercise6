ff71b060af85bfbc23b53e78059f04f2aa26acc8
agustuscaeza <b032110212@student.utem.edu.my>
2023-06-14 22:26:59 +0800

    Update Translate.java

--- lab13execise6/src/lab13exercise6/Translate.java
+++ lab13execise6/src/lab13exercise6/Translate.java
@@ -9,33 +9,10 @@ public class Translate {
 		if (translateTo == 1)
 		{
 		   String translatedWord =  "Ada apa?, " + "? ما أخبارك , " + "뭐야 ";
-		   //return EnglishWord;
 		   
-		   /*String BahasaMalaysia = "Terima Kasih";
-		   //return BahasaMalaysia;
-		   
-		   String Arabic = " شكرا لك";
-		   //return Arabic;
-		   
-		   String Hangul = "감사합니다 ";
-		   return  Hangul;*/
 		   return translatedWord;
 		} 
-		/*if (translateTo == 2)
-		{
-		   String BahasaMalaysia = "Terima Kasih";
-		   return BahasaMalaysia;
-		}
-		if (translateTo == 3)
-		{
-		   String Arabic = " شكرا لك";
-		   return Arabic;
-		}
-		if (translateTo == 4)
-		{
-		   String Hangul = "감사합니다 ";
-		   return Hangul;
-		}*/
+		
 		else
 		{
 			return "The language is not available";
